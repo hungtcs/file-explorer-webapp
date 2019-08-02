23faafa59d892214b91f0c63d0175058a74560de
刘文杰 <lwenjie@hirisun.com>
2019-08-02 22:16:57 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'file-explorer-webapp';
+}
